a214197a2f4f8a15f85e0134ae4241d2f23521d4
Taral Babubhai Patel <taralbabubhaipatel@MacBook-Air.local>
2025-06-07 15:05:17 +0530

    authentication with jwt

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const connectDB = require("./config/db");
 require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+const errorHandler = require("./middleware/errorHandler");
 
 connectDB();
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/orders", orderRoutes);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");

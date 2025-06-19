fb94047c88566847b6aaa3295f7ce01ffdc9ccc6
Taral92 <taru.patel37225@gmail.com>
2025-06-19 17:08:37 +0530

    bug fixes

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -2,19 +2,34 @@ const express = require("express");
 const router = express.Router();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-router.post("/create-payment-intent", async (req, res) => {
-  const { amount } = req.body;
+router.post("/create-checkout-session", async (req, res) => {
+  const { cartItems } = req.body;
 
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
+    if (!cartItems || cartItems.length === 0) {
+      return res.status(400).json({ error: "Cart is empty" });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: cartItems.map((item) => ({
+        price_data: {
+          currency: "inr",
+          product_data: {
+            name: item.name || "Unnamed Product",
+          },
+          unit_amount: Number(item.basePrice) * 100 || 1000,
+        },
+        quantity: item.quantity || 1,
+      })),
+      mode: "payment",
+      success_url: "http://localhost:5173/success",
+      cancel_url: "http://localhost:5173/cancel",
     });
-    res.status(200).json({ clientSecret: paymentIntent.client_secret });
+
+    res.status(200).json({ url: session.url });
   } catch (error) {
+    console.error("Stripe error:", error.message);
     res.status(500).json({ error: error.message });
   }
 });

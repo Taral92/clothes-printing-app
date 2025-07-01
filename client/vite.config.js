c226980f93fe445e3bbe2b2d13884e060d74ede1
Taral92 <taru.patel37225@gmail.com>
2025-07-01 19:16:30 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss  from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    host: true,
+  },
+  // 👇 important part for fallback on Vercel
+  preview: {
+    headers: {
+      'Cache-Control': 'no-cache',
+    }
+  }
 })

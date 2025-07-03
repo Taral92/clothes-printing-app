f5c7e5cf286224ce1cc16e041f3880b26983f20c
Taral92 <taru.patel37225@gmail.com>
2025-07-03 13:38:45 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -37,6 +37,6 @@ app.use("/api/cart", cartRoutes);
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
